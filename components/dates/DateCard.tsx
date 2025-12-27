114fdb03df4286ddbddf240a261da489d402e31c
satori-lunar <elliotmccormick@satori-lunar.com>
2025-12-26 19:28:56 -0500

    Fix DateNight type import in DateCard component

--- components/dates/DateCard.tsx
+++ components/dates/DateCard.tsx
@@ -3,6 +3,7 @@
 import { useState } from 'react'
 import { useAppStore } from '@/lib/store'
 import { Calendar, MapPin, Video } from 'lucide-react'
+import type { DateNight } from '@/types'
 
 interface DateCardProps {
   date: DateNight
